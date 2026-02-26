2f2cfa2a5b9e8eb11bfd88169014cc2769aa092a
Adnan Hussain <7xy.plane@gmail.com>
2026-02-26 18:33:57 +0530

    feat: inertia with auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
+use App\Http\Controllers\MailLogController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\MailLogController::class, 'index'])->name('dashboard');
-Route::post('/settings', [\App\Http\Controllers\MailLogController::class, 'updateSettings'])->name('settings.update');
+Route::middleware('auth')->group(function () {
+    Route::get('/', [MailLogController::class, 'index'])->name('dashboard');
+    Route::post('/settings', [MailLogController::class, 'updateSettings'])->name('settings.update');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';

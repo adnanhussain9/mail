b2cbbd10fa9595e917315e465dff8b8ddc2443de
Adnan Hussain <7xy.plane@gmail.com>
2026-02-26 17:11:22 +0530

    auth added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,25 @@
 <?php
 
+use App\Http\Controllers\MailLogController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\MailLogController::class, 'index'])->name('dashboard');
-Route::post('/settings', [\App\Http\Controllers\MailLogController::class, 'updateSettings'])->name('settings.update');
+
+Route::get('/', [MailLogController::class, 'index'])->name('dashboard');
+Route::post('/settings', [MailLogController::class, 'updateSettings'])->name('settings.update');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', [MailLogController::class, 'index'])->name('dashboard');
+    Route::post('/settings', [MailLogController::class, 'updateSettings'])->name('settings.update');
+
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';

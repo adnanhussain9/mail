c4633b52d2a263928723aa104e792c1d566a9358
Adnan Hussain <7xy.plane@gmail.com>
2026-02-27 15:51:16 +0530

    console command added

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command('mail:process-sheet')
+    ->everyMinute()
+    ->appendOutputTo(storage_path('logs/sheet_process.log'));
